75931774decec3189ccedabb494ba3bf077aeedc
Elliot Escovitch <dev.escovitch.ri@gmail.com>
2024-08-25 13:31:18 -0500

    add custom pipe and delete service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/pokemon'),
-    PokemonModule]
+    PokemonModule,
+    CommonModule]
 })
 export class AppModule {}
